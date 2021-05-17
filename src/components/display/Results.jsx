8f38424b87b90eff427731216c189873229910a5
Eion Nelson <ecnelson1@live.com>
2021-05-17 06:30:19 -0700

    aded results display component and api-utils to make a request.

---
+++ src/components/display/Results.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Results({results}) {
+    return (
+        <div>
+            <h1>Request Results:</h1>
+            <p>{results}</p>
+            
+        </div>
+    )
+}
+
+Results.propTypes = {
+    results: PropTypes.any.isRequired
+
+}
+
+export default Results
+
+
